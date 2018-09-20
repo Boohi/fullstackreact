19ccee15bb4280f0e91aedd80a296ffedf742700
Otto Mäenpää <otto.maenpaa@gmail.com>
2018-09-20 17:21:00 +0300

    added authentication restrictions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 require("./models/User");
 require("./services/passport");
 
@@ -12,17 +13,28 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-
-//Setup cookies and initialize passport
-app.use(cookieSession({
-    maxAge: 30*24*60*60*1000,
-    keys: [keys.cookieKey]
-}));
+//Setup middleware
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //Routes
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 //Assign port to listen to
 const PORT = process.env.PORT || 5000;
