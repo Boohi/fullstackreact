8935bdeaf547e87f1d3c336777d5e58eb43c3c71
Otto Mäenpää <otto.maenpaa@gmail.com>
2018-09-25 14:39:16 +0300

    minor edit to email 'from' field

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,7 +11,7 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('joona@haista-vittu.XD');
+        this.from_email = new helper.Email('no-reply@lss.ai');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
